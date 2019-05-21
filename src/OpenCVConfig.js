250b4e58b395a0c9790128b6b490b7aeb3ed2b3d
Simon Tsang (CTHK) <simon.tsang@ctint.com>
2019-05-21 19:01:14 +0800

    Add screen shot interval settings for customizing

--- src/OpenCVConfig.js
+++ src/OpenCVConfig.js
@@ -1,8 +1,8 @@
 const OpenCVConfig = {
     // Webcam capture
     // rtsp example: "rtsp://admin:P@ssword654321@192.168.1.20:554/h264/1/main/av_stream"
-    cameraConnectionString: 0, // 0 = device cam, could be a rtsp (above example)
-    
+    cameraConnectionString: "rtsp://admin:P@ssword654321@192.168.1.20:554/h264/1/main/av_stream", // 0 = device cam, could be a rtsp (above example)
+    screenshotInterval: 1000, // in ms
     // Face recognition for static picture
     imageFileName: '1080pCrowd', // any image file name in ./TestFace
     imageFileExtension: 'jpg', // depends on the extension of the image
